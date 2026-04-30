df73466d0614c685aa5c529de57ec602e9f1e44d
Aki Sato <csat9038@uni.sydney.edu.au>
2026-04-30 20:43:54 +1000

    chore: remove boilerplate JS files, keeping folder structure only

--- backend/middleware/admin.middleware.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-  if (req.user?.role !== 'admin') {
-    return res.status(403).json({ success: false, error: 'Admin access required' });
-  }
-  next();
-};
-
-module.exports = adminMiddleware;
